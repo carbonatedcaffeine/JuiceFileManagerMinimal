c756e349a3212bd790c26ee9e6ab435da0349395
Ender <31677287+EnderNightLord-ChromeBook@users.noreply.github.com>
2018-11-09 19:59:56 +1300

    Update juicefilemanager3.2.py

--- juicefilemanager3.2.py
+++ juicefilemanager3.2.py
@@ -12,7 +12,7 @@ print(' Done ')
 def Update():
     os.system("sudo apt-get update")
     os.system("sudo apt-get install wget")
-    os.system("wget https://github.com/EnderNightLord-ChromeBook/JuiceFileManagerMINIMAL/juicefilemanager3.2.py")
+    os.system("wget https://raw.githubusercontent.com/EnderNightLord-ChromeBook/JuiceFileManagerMinimal/master/juicefilemanager3.5.py")
     print('if not found no update')
 
 def Read():        #For reading files
