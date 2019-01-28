fb2c7c72fadc32405a191f4a5d366062417443b7
Ender <31677287+EnderNightLord-ChromeBook@users.noreply.github.com>
2019-01-28 17:22:14 +1300

    Update and rename juicefilemanager3.5.py to juicefilemanager3.6.py (#11)

--- juicefilemanager3.6.py
+++ juicefilemanager3.6.py
@@ -18,6 +18,8 @@ print(' Done ')
 
 def OpenWeb():
 	webbrowser.open('https://github.com/EnderNightLord-ChromeBook/JuiceFileManagerMinimal', new=2)
+	webbrowser.open('https://endernightlord-chromebook.github.io/EnderNightLord-Web.io/', new=3)
+	
 def Update():
     os.system("sudo apt-get update")
     os.system("sudo apt-get install wget")
