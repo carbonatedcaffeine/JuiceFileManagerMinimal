191f9346b71193aa11d07346ea64991bd342be7f
Ender <31677287+EnderNightLord-ChromeBook@users.noreply.github.com>
2019-02-10 11:15:18 +1300

    Add files via upload

---
+++ assets/Information.py
@@ -0,0 +1,8 @@
+print('''
+Detailed Build Information
+=======================================
+Made On:GNU/LINUX
+Date Created: 31/1/2019
+Thanks To:Python Fans
+=======================================
+''')
