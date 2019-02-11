8c6a750d94cb044e609055f9fe94958e26fc169a
EnderNightLord <31677287+EnderNightLord-ChromeBook@users.noreply.github.com>
2019-02-11 18:44:56 +1300

    Update OpenWeb.py (#16)
    * Update OpenWeb.py
    
    * Update OpenWeb.py

--- assets/OpenWeb.py
+++ assets/OpenWeb.py
@@ -1,4 +1,25 @@
 import webbrowser
 
-webbrowser.open('https://github.com/EnderNightLord-ChromeBook/JuiceFileManagerMinimal', new=2)
-webbrowser.open('https://endernightlord-chromebook.github.io/EnderNightLord-Web.io/', new=3)
+ans = input('''
+Juice File Manager 4.0 Websites
+=======================================
+Which Website Would You Like To Go To?
+
+1. JFM Home Page
+
+2. Enders Blog
+
+3. Juice Programming Page
+
+#: ''')
+
+if ans == '1':
+  webbrowser.open('https://github.com/EnderNightLord-ChromeBook/JuiceFileManagerMinimal', new=2)
+
+if ans == '2':
+  webbrowser.open('https://endernightlord-chromebook.github.io/EnderNightLord-Web.io/', new=3)
+
+if ans == '3':
+  webbrowser.open('https://sites.google.com/view/juicefilemanager/home/', new=3)
+  
+  
