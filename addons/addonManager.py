05ab11008fa57b87edc17d0fa1c4428509e16391
EnderNightLord <31677287+EnderNightLord-ChromeBook@users.noreply.github.com>
2019-02-11 19:58:06 +1300

    Update addonManager.py

--- addons/addonManager.py
+++ addons/addonManager.py
@@ -1,5 +1,157 @@
+import os
+
 ans = input('''
 ==============================
 JuiceFileManager Addon Manager
 ==============================
+
+What Kind Of Install Do You Have?
+
+1. ~/Downloads
+
+2. /root
+
+3. /
+
+4. ~/
+
+#: ''')
+
+if ans == '1':
+  print('''
+==============================
+JuiceFileManager Addon Manager
+==============================
+welcome to the addon manager!
+please wait till we import some
+packages from github...
 ''')
+  os.system("mkdir ~/Downloads/JFM/Addons")
+  os.system("wget https://raw.githubusercontent.com/EnderNightLord-ChromeBook/JuiceFileManagerMinimal/Addon_Manager_(Update)/addon_assets/AdminOptions.py -P ~/Downloads/JFM/Addons/ ")
+  ans = input('''
+==============================
+JuiceFileManager Addon Manager
+==============================
+would you like to install any
+addons?
+
+1. Admin Options
+
+2. No (Will Delete Package)
+
+#: ''')
+  if ans == '1':
+    os.system("cp ~/Downloads/JFM/Addons/AdminOptions.py ~/Downloads/JFM/Assets")
+    os.system("rm ~/Downloads/JFM/Addons/AdminOptions.py")
+    os.system("rm -R ~/Downloads/JFM/Addons")
+    print('Done')
+  
+  if ans == '2':
+    os.system("rm ~/Downloads/JFM/Addons/AdminOptions.py")
+    os.system("rm -R ~/Downloads/JFM/Addons")
+    
+    
+if ans == '2':
+  print('''
+==============================
+JuiceFileManager Addon Manager
+==============================
+welcome to the addon manager!
+please wait till we import some
+packages from github...
+''')
+  os.system("mkdir /root/JFM/Addons")
+  os.system("wget https://raw.githubusercontent.com/EnderNightLord-ChromeBook/JuiceFileManagerMinimal/Addon_Manager_(Update)/addon_assets/AdminOptions.py -P /root/JFM/Addons/ ")
+  ans = input('''
+==============================
+JuiceFileManager Addon Manager
+==============================
+would you like to install any
+addons?
+
+1. Admin Options
+
+2. No (Will Delete Package)
+
+#: ''')
+  if ans == '1':
+    os.system("cp /root/JFM/Addons/AdminOptions.py /root/JFM/Assets")
+    os.system("rm /root/JFM/Addons/AdminOptions.py")
+    os.system("rm -R ~/Downloads/JFM/Addons")
+    print('Done')
+  
+  if ans == '2':
+    os.system("rm /root/JFM/Addons/AdminOptions.py")
+    os.system("rm -R /root/JFM/Addons")
+    
+if ans == '3':
+  print('''
+==============================
+JuiceFileManager Addon Manager
+==============================
+welcome to the addon manager!
+please wait till we import some
+packages from github...
+''')
+  os.system("mkdir /JFM/Addons")
+  os.system("wget https://raw.githubusercontent.com/EnderNightLord-ChromeBook/JuiceFileManagerMinimal/Addon_Manager_(Update)/addon_assets/AdminOptions.py -P /JFM/Addons/ ")
+  ans = input('''
+==============================
+JuiceFileManager Addon Manager
+==============================
+would you like to install any
+addons?
+
+1. Admin Options
+
+2. No (Will Delete Package)
+
+#: ''')
+  if ans == '1':
+    os.system("cp /JFM/Addons/AdminOptions.py /JFM/Assets")
+    os.system("rm /JFM/Addons/AdminOptions.py")
+    os.system("rm -R /JFM/Addons")
+    print('Done')
+  
+  if ans == '2':
+    os.system("rm /JFM/Addons/AdminOptions.py")
+    os.system("rm -R /JFM/Addons")
+    
+if ans == '4':
+  print('''
+==============================
+JuiceFileManager Addon Manager
+==============================
+welcome to the addon manager!
+please wait till we import some
+packages from github...
+''')
+  os.system("mkdir ~/JFM/Addons")
+  os.system("wget https://raw.githubusercontent.com/EnderNightLord-ChromeBook/JuiceFileManagerMinimal/Addon_Manager_(Update)/addon_assets/AdminOptions.py -P ~/JFM/Addons/ ")
+  ans = input('''
+==============================
+JuiceFileManager Addon Manager
+==============================
+would you like to install any
+addons?
+
+1. Admin Options
+
+2. No (Will Delete Package)
+
+#: ''')
+  if ans == '1':
+    os.system("cp ~/JFM/Addons/AdminOptions.py ~/JFM/Assets")
+    os.system("rm ~/JFM/Addons/AdminOptions.py")
+    os.system("rm -R ~/JFM/Addons")
+    print('Done')
+  
+  if ans == '2':
+    os.system("rm ~/JFM/Addons/AdminOptions.py")
+    os.system("rm -R ~/JFM/Addons")
+    
+ 
+
+
+
+
