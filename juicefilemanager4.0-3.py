2014a5177774f03fc34f5cace5033408a70fe3f4
EnderNightLord <31677287+EnderNightLord-ChromeBook@users.noreply.github.com>
2019-02-11 19:21:05 +1300

    Update juicefilemanager4.0-3.py

--- juicefilemanager4.0-3.py
+++ juicefilemanager4.0-3.py
@@ -51,6 +51,7 @@ def Extra():
 run=1
 while(run==1):
     print('''
+=======================================
 Juice File Manager 4.0
 =======================================''')
     dec=int(input('''1.Read a file
@@ -66,6 +67,7 @@ Juice File Manager 4.0
 11. Open a program
 12. Exit
 
+=======================================
 Other
 =======================================
 13. Build Information
