d388a770f332cf409e5dfeda7f2d2106a82c7460
EnderNightLord <31677287+EnderNightLord-ChromeBook@users.noreply.github.com>
2019-03-19 12:07:12 +1300

    Update Copy.py

--- assets/Copy.py
+++ assets/Copy.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 
-path1=input('Enter the location of File to copy or rename:')
+path1=input('Enter Location Of File to copy or rename:')
 path2=input('Enter the location to copy:')
 shutil.copy(path1,path2)
 print('File copied')
