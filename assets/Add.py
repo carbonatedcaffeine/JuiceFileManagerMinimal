24ebb053a665845a5f385cccea2a7dae4540a648
EnderNightLord <31677287+EnderNightLord-ChromeBook@users.noreply.github.com>
2019-03-19 12:06:32 +1300

    Update Add.py

--- assets/Add.py
+++ assets/Add.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 
-path=input("Enter the file location:")
+path=input("Enter Location Of File:")
 text=input("Enter the text to write:")
 file=open(path,"a")
 file.write('\n'+text)
