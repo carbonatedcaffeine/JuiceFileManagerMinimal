2e73b2ffb7448b06de8de38d1dd3dbca2b8719ff
EnderNightLord <31677287+EnderNightLord-ChromeBook@users.noreply.github.com>
2019-03-19 12:09:55 +1300

    Update Read.py

--- assets/Read.py
+++ assets/Read.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 
-path=input("Enter the location of file to read:")
+path=input("Enter Location Of File to read:")
 file=open(path,"r")
 print(file.read()) 
 input('Press Enter...')  
