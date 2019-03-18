381cae756ac73ba748812ee62c77e2ca748d9142
EnderNightLord <31677287+EnderNightLord-ChromeBook@users.noreply.github.com>
2019-03-19 12:07:33 +1300

    Update Delete.py

--- assets/Delete.py
+++ assets/Delete.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 
-path=input("Enter the location of file to be write or create:")
+path=input("Enter Location Of File to be write or create:")
 if os.path.exists(path):      # checks if the file exists
     print('File Found')     #For existing file
     os.remove(path)          #os.remove(file path) is used to delete
