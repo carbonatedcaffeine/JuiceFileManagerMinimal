d2c67c438280b065730b30a464553c68b649a864
EnderNightLord <31677287+EnderNightLord-ChromeBook@users.noreply.github.com>
2019-03-19 12:09:08 +1300

    Update Move.py

--- assets/Move.py
+++ assets/Move.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 
-path1=input('Enter the location of File to move or rename:')
+path1=input('Enter Location Of File to move or rename:')
 mr=int(input('1.Rename \n2.Move \n'))
 if mr==1:
     path2=input('Enter the resulting location with resulting file name:')
