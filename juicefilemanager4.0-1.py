846add2c60e8b1d285b570ea3fe0fef05d42f740
EnderNightLord <31677287+EnderNightLord-ChromeBook@users.noreply.github.com>
2019-03-29 08:26:43 +1300

    Update juicefilemanager4.0-1.py

--- juicefilemanager4.0-1.py
+++ juicefilemanager4.0-1.py
@@ -62,10 +62,6 @@ def Information():
 # Imports the extra asset to show some extras
 def Extra():
     os.system("python3 ~/Downloads/JFM/Assets/Extra.py")
-
-def ShowReadme():
-    os.system("pyhton3 ~/Downloads/JFM/Assets/ShowReadme.py")
-
 run=1
 while(run==1):
     print('''
@@ -126,8 +122,6 @@ Other
         Update()
     if dec==16:
         OpenWeb()
-    if dec==17:
-	ShowReadme()
 
 
 
