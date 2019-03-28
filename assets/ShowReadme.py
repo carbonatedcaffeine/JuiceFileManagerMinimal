00fd5431979f5e6494d7a500e28e7d8649d263e5
Ender <camden.o.b@gmail.com>
2019-03-28 05:33:57 +0000

    New command: show readme

---
+++ assets/ShowReadme.py
@@ -0,0 +1,60 @@
+print('''
+JuciceFileManagerMinimal
+a file manager made from just python code and made for linux minimal
+
+How To Install
+Download installJuice.py
+
+python3 installJuice.py And Follow Instructions
+
+to launch juicefilemanager type python3 juicefilemanager4.0.py (in the dir you installed into)
+
+Updating Modules
+in juicefilemanager type 15 and go follow instructions...
+
+Notes On Updating!!! (Importent)
+if an update on github is made. It may take up to 5 minutes to the update to sync into github. So an update may not be installed instantly.
+
+Update Information
+--> Latest Version: 4.0
+
+--> Latest Version Codename: Draco Dragonfruit
+
+--> Latest Minor Update: NONE
+
+--> Latest Beta Release: NONE
+
+--> Next Milestone Release: 4.5
+
+--> Next Milestone Release Codename: Blooper Blueberry
+
+Update Notes: 4.0
+--> Options Are Now Individual Files! Bettter For Updates.
+
+--> installJuice.py Will Install JuiceFileManager!
+
+--> juicefilemanager4.0-1, -2 and -3 are for branches... They are for different dirs... but are renamed in the installer.
+
+--> Update Module Will Be Updated To Update Modules One By One To Make Things Easy (This Will Also Update Your Update File)
+
+This program can be used for:
+--> For reading files
+
+--> For writing or creating files
+
+--> Adding text to a file
+
+--> Deleting a File
+
+--> Listing files in a directory
+
+--> Checking file or directory presence
+
+--> For moving or renameing file
+
+--> For copying
+
+--> For creating directory
+
+--> For removing Directory
+''')
